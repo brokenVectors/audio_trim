9203ec5e1a2ead683c6f47c11c24016d92e109b0
brokenVectors <brokenvectors@gmail.com>
2020-04-30 19:59:54 +0200

    You can now select one folder containing multiple subfolders, and all their MP3s will be trimmed!

--- main.py
+++ main.py
@@ -2,12 +2,56 @@ from pydub import AudioSegment
 import tkinter as tk
 import os
 import tkinter.font as tkFont
+import ntpath
 from tkinter.filedialog import askdirectory
+def TruncateFile(inputLocation, outputLocation):
+    print("Received:" + inputLocation);
+    song = AudioSegment.from_mp3(inputLocation)
+    length = len(song)
+    newLength = length / 3
+    truncated = song[:newLength]
+    truncated.export(outputLocation, format="mp3")
 
+def trimAlbum(foldername):
+    trimmedPath = foldername + "/trimmed"
+    if (not os.path.isdir(trimmedPath)):
+        os.mkdir(trimmedPath)
+    for filename in os.listdir(foldername):
+        if filename.endswith(".mp3"):
+            inputPath = foldername + "/" + filename
+            outputPath = trimmedPath + "/" + filename
+            print("Output: " + outputPath)
+            print("Input: " + inputPath)
+            print("Filename: " + filename)
+
+
+            TruncateFile(inputPath, outputPath);
 def onTrimPress():
     print('Pressed')
-    filename = askdirectory()
-    os.mkdir("songs")
+    foldername = askdirectory()
+
+
+
+    print(foldername)
+    for filename in os.listdir(foldername):
+        filePath = foldername + "/" + filename
+        if os.path.isdir(filePath):
+            trimAlbum(filePath)
+        else:
+            if filename.endswith(".mp3"):
+                trimmedPath = foldername + "/trimmed"
+                if (not os.path.isdir(trimmedPath)):
+                    os.mkdir(trimmedPath)
+                inputPath = foldername + "/" + filename
+                outputPath = trimmedPath + "/" + filename
+                print("Output: " + outputPath)
+                print("Input: " + inputPath)
+                print("Filename: " + filename)
+
+                TruncateFile(inputPath, outputPath);
+
+
+
 def tkLoop():
     root.geometry("200x100")
     root.resizable(0, 0)
@@ -23,11 +67,5 @@ root = tk.Tk()
 fontStyle = tkFont.Font(family="Corbel Light", size=16)
 tkLoop()
 root.mainloop()
-def TruncateFile(inputLocation, outputLocation):
 
-    song = AudioSegment.from_mp3(inputName)
-    length = len(song)
-    newLength = length / 3
-    truncated = song[:newLength]
-    truncated.export(outputName, format="mp3")
 
